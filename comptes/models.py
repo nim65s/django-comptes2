3d52b9bf50100373e3085728cba7dabcdbb4e925
Guilhem Saurel <guilhem.saurel@gmail.com>
2014-03-23 22:33:34 +0900

    systèmes unifiés d’url dans les modèles et de validation des objets

--- comptes/models.py
+++ comptes/models.py
@@ -8,6 +8,21 @@ from django.db.models import BooleanField, DateTimeField, DecimalField, ForeignK
 from django.utils.html import mark_safe
 
 
+class CompteModel(Model):
+    class Meta:
+        abstract = True
+
+    def get_absolute_url(self):
+        return self.get_list_url()  # temporaire
+
+    def get_list_url(self):
+        return reverse(self.__class__.__name__.lower() + '_list')
+
+    def get_validate_urls(self):
+        nom = self.__class__.__name__.lower()
+        return (reverse(nom + '_ok', kwargs={'pk': self.pk}), reverse(nom + '_ko', kwargs={'pk': self.pk}))
+
+
 class PortailUser(Model):
     user = ForeignKey(User, unique=True)
     solde = DecimalField(max_digits=8, decimal_places=2, default=0)
@@ -39,7 +54,7 @@ class PortailUser(Model):
         ordering = ["-solde"]
 
 
-class Creance(Model):
+class Creance(CompteModel):
     creancier = ForeignKey(PortailUser, verbose_name=u"Créancier")
     montant = DecimalField(max_digits=8, decimal_places=2)
     description = TextField()
@@ -64,13 +79,14 @@ class Creance(Model):
         return mark_safe(", ".join(liste))
 
     def nombre_parts(self):
-        return self.dette_set.all().aggregate(s=Sum('parts'))['s']
+        return self.dette_set.all().aggregate(s=Sum('parts'))['s'] or 0
 
     def valeur_part(self):
-        return self.montant / self.nombre_parts()
-
-    def get_absolute_url(self):
-        return reverse('creance_list')
+        nombre_parts = self.nombre_parts()
+        if nombre_parts:
+            return self.montant / nombre_parts
+        else:
+            return self.montant
 
     def __unicode__(self):
         return u"Créance de %s de %.2f € le %s" % (self.creancier, self.montant, self.moment)
@@ -79,7 +95,7 @@ class Creance(Model):
         ordering = ["moment"]
 
 
-class Dette(Model):
+class Dette(CompteModel):
     creance = ForeignKey(Creance, verbose_name=u"Créance")
     debiteur = ForeignKey(PortailUser, verbose_name=u"Débiteur")
     parts = IntegerField(u"Nombre de parts", default=1)
@@ -96,9 +112,6 @@ class Dette(Model):
     def valeur(self):
         return self.parts * self.creance.valeur_part()
 
-    def get_absolute_url(self):
-        return reverse('dette_list')
-
     def __unicode__(self):
         return u"%s doit %.2f € à %s" % (self.debiteur, self.valeur(), self.creance.creancier)
 
@@ -107,7 +120,7 @@ class Dette(Model):
         ordering = ["creance", "parts"]
 
 
-class Remboursement(Model):
+class Remboursement(CompteModel):
     crediteur = ForeignKey(PortailUser, verbose_name=u"Créditeur", related_name='debits')
     credite = ForeignKey(PortailUser, verbose_name=u"Crédité", related_name='credits')
     montant = DecimalField(max_digits=8, decimal_places=2)
@@ -116,6 +129,7 @@ class Remboursement(Model):
     valide_credite = NullBooleanField(default=None)
 
     def validable(self, user):
+        print self.crediteur.user, self.credite.user, user
         return self.crediteur.user == user or self.credite.user == user
 
     def save(self, *args, **kwargs):
@@ -123,9 +137,6 @@ class Remboursement(Model):
 
         self.credite.maj_solde()
         self.crediteur.maj_solde()
-
-    def get_absolute_url(self):
-        return reverse('remboursement_list')
 
     def __unicode__(self):
         return u"%s a remboursé %.2f € à %s" % (self.crediteur, self.montant, self.credite)
--- comptes/templatetags/comptes.py
+++ comptes/templatetags/comptes.py
@@ -5,6 +5,9 @@ from django import template
 register = template.Library()
 
 
-@register.filter
-def peut_valider(user, truc):
-    return truc.validable(user)
+@register.simple_tag
+def valider(user, truc):
+    if truc.validable(user):
+        return u'<a href="%s">Oui</a> <a href="%s">Non</a>' % truc.get_validate_urls()
+    else:
+        return ''
